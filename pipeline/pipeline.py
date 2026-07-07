989efa10e886c16a76c1bda9d386441f7b4907e2
DONGHUI ZHOU <donghuizhou12@gmail.com>
2026-07-07 20:55:51 +0000

    pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,11 @@
+import sys 
+import pandas as pd
+print('argument', sys.argv)
+print('Hello pipeline!')
+print(sys.argv[0])
+month=sys.argv[1]
+print(month)
+df =pd.DataFrame({'A':[1,2,3], 'B':[4,5,6]})
+df['month']=month
+print(df.head())
+df.to_parquet(f'output_{month}.parquet')
