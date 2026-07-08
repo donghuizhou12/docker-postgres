3a823f04cd43f3c2bb6dfc9b8b6801bc3a591c4e
DONGHUI ZHOU <donghuizhou12@gmail.com>
2026-07-08 21:34:56 +0000

    pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -3,9 +3,13 @@ import pandas as pd
 print('argument', sys.argv)
 print('Hello pipeline!')
 print(sys.argv[0])
-month=sys.argv[1]
+month=int(sys.argv[1])
 print(month)
 df =pd.DataFrame({'A':[1,2,3], 'B':[4,5,6]})
 df['month']=month
 print(df.head())
-df.to_parquet(f'output_{month}.parquet')
+df.to_parquet (f'output_{month}.parquet')
+print(df.month.dtype,df.columns)
+# for dc in df.columns:
+#     print(dc.dtype)
+print(type(df.columns))
